0e3ff419abe8dbc3aad4a3f427b22c0d410dbacc
RawanCSC <relgohary@my.waketech.edu>
2026-04-28 15:27:01 -0400

    Update TestLoanClassTemplate.java
    I add the while (myLoanAmount >= myMonthlyPayment) and the if (myLoanAmount < myMonthlyPayment).

--- TestLoanClassTemplate.java
+++ TestLoanClassTemplate.java
@@ -97,21 +97,29 @@ public class TestLoanClassTemplate {
 
     // count the month
     month++;
-}
+	}
 	  
 	  while (myLoanAmount >= myMonthlyPayment) {
 		  // to do B2 same as B1
 		  // caculate the new month's interest based on myLoanAmount and myMonthlyInterestRate
+		  intrest = myLoanAmount * ( myMonthlyInterestRate / 12 ); 
 		  // caculate the principal paid this month from myMonthlyPayment minus the new month's interest
+		  principal = myMonthlyPayment - interest;
 		  // caculate the new myLoanAmount after minus this month's principal
+		  myLoanAmount -= principal; 
 		  // add the interest paid this month to totalInterest
+		  totalInterest += interest;
 		  // increment the month count
+		  month++; 
 	  }
 	  if (myLoanAmount < myMonthlyPayment) {
 		  // to do
 		  // caculate the new month's interest based on myLoanAmount and myMonthlyInterestRate
+		  intrest = myLoanAmount * ( myMonthlyInterestRate / 12 );
 		  // add the interest paid this month to totalInterest
+		  totalInterest += interest;
 		  // increment the month count
+		  month++; 
 	  }
 	  double interestSaved = l.getTotalInterest() - totalInterest;
 	  System.out.printf("Total interest $%.2f%nTotal interest saved $%.2f%nTotal months %d %n",
